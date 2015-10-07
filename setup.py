ef88c3f3bd5c3d754b23526bc0160a842654b0f2
Chris Seymour <chris.j.seymour@hotmail.com>
2015-10-07 22:22:24 +0100

    added flag for version
    version is currently declared in both uwsgitop and setup, it would
    be better to have a single source.

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from distutils.core import setup
 
 setup(name='uwsgitop',
-        version='0.8',
-        description='uWSGI top-like interface',
-        license='MIT',
-        scripts=['uwsgitop'],
-        install_requires = ['simplejson']
-        )
+      version='0.8',
+      description='uWSGI top-like interface',
+      license='MIT',
+      scripts=['uwsgitop'],
+      install_requires=['simplejson'])
